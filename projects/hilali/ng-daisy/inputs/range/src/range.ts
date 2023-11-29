2a380ef967ec266a345093bbff8cbc8ac5a16be6
Abderrahmane HILALI <h.abderrahmane.contact@gmail.com>
2023-11-29 16:25:28 +0100

    enhancement:range/toggle use BaseInput

--- projects/hilali/ng-daisy/inputs/range/src/range.ts
+++ projects/hilali/ng-daisy/inputs/range/src/range.ts
@@ -1,32 +1,71 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 import {
-  ColorManager,
-  DaiColor,
-  DaiSize,
-  SizeManager,
-} from '@hilali/ng-daisy/common';
+  Directive,
+  ElementRef,
+  Input,
+  Optional,
+  Renderer2,
+  Self,
+} from '@angular/core';
+import { FormControlDirective } from '@angular/forms';
+import { DaiColor, DaiSize, SizeManager } from '@hilali/ng-daisy/common';
+import { BaseInput } from '../../base-input';
 
 @Directive({
   selector: 'input[type="range"], input[type="range"][daiRange]',
   standalone: true,
   providers: [{ provide: SizeManager, useClass: SizeManager }],
 })
-export class Range extends ColorManager {
-  @Input() set size(size: DaiSize) {
-    this.sizeManager.setSize(this.el.nativeElement, size, 'range');
-  }
-
-  @Input() set color(v: DaiColor) {
-    this.setColor(this.el.nativeElement, v, 'range');
-  }
-
+export class Range extends BaseInput {
   constructor(
-    private el: ElementRef<HTMLElement>,
-    private renderer: Renderer2,
-    private readonly sizeManager: SizeManager
+    el: ElementRef<HTMLInputElement>,
+    renderer: Renderer2,
+    @Optional() @Self() control: FormControlDirective
   ) {
-    super(renderer);
-    this.sizeManager.setDefault('md');
-    this.renderer.addClass(this.el.nativeElement, 'range');
+    super(el, renderer, control);
   }
+
+  protected override get _defaultClasses(): string[] {
+    return ['range', 'range-neutral', 'range-md'];
+  }
+  protected override updateColor(color: DaiColor): void {
+    this.resetColor();
+    this.renderer.addClass(this.el.nativeElement, `range-${color}`);
+  }
+  protected override updateSize(size: DaiSize): void {
+    this.el.nativeElement.classList.remove(
+      'range-xs',
+      'range-sm',
+      'range-md',
+      'range-lg'
+    );
+    this.renderer.addClass(this.el.nativeElement, `range-${size}`);
+  }
+
+  protected override updateStatus(
+    status: 'success' | 'error' | 'default'
+  ): void {
+    this.resetColor();
+    if (status === 'success' || status === 'error') {
+      this.renderer.addClass(this.el.nativeElement, `range-${status}`);
+    }
+  }
+
+  private resetColor(): void {
+    this.el.nativeElement.classList.remove(
+      'range-primary',
+      'range-secondary',
+      'range-success',
+      'range-warning',
+      'range-error',
+      'range-neutral',
+      'range-accent',
+      'range-info'
+    );
+  }
+
+  /**
+   * this element has no variants
+   */
+  @Input({ alias: 'variant' }) _variant_: 'ghost' | 'bordered' = 'bordered';
+  protected override updateVariant(variant: 'ghost' | 'bordered'): void {}
 }
--- projects/hilali/ng-daisy/inputs/toggle/src/toggle.ts
+++ projects/hilali/ng-daisy/inputs/toggle/src/toggle.ts
@@ -1,14 +1,66 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  Optional,
+  Renderer2,
+  Self,
+} from '@angular/core';
+import { FormControlDirective } from '@angular/forms';
+import { DaiColor, DaiSize } from '../../../../../../dist/hilali/ng-daisy';
+import { BaseInput } from '../../base-input';
 
 @Directive({
   selector: 'input[type="checkbox"][daiToggle]',
   standalone: true,
 })
-export class Toggle {
-  constructor(
-    private el: ElementRef<HTMLElement>,
-    private renderer: Renderer2
-  ) {
-    this.renderer.addClass(this.el.nativeElement, 'toggle');
+export class Toggle extends BaseInput {
+  protected override get _defaultClasses(): string[] {
+    return ['toggle', 'toggle-neutral', 'toggle-md'];
   }
+  protected override updateColor(color: DaiColor): void {
+    this.resetColor();
+    this.renderer.addClass(this.el.nativeElement, `toggle-${color}`);
+  }
+  protected override updateSize(size: DaiSize): void {
+    this.el.nativeElement.classList.remove(
+      'toggle-xs',
+      'toggle-sm',
+      'toggle-md',
+      'toggle-lg'
+    );
+    this.renderer.addClass(this.el.nativeElement, `toggle-${size}`);
+  }
+  protected override updateStatus(
+    status: 'success' | 'error' | 'default'
+  ): void {
+    this.resetColor();
+    if (status === 'success' || status === 'error') {
+      this.renderer.addClass(this.el.nativeElement, `toggle-${status}`);
+    }
+  }
+
+  constructor(
+    el: ElementRef<HTMLInputElement>,
+    renderer: Renderer2,
+    @Optional() @Self() formControl: FormControlDirective
+  ) {
+    super(el, renderer, formControl);
+  }
+
+  private resetColor(): void {
+    this.el.nativeElement.classList.remove(
+      'toggle-primary',
+      'toggle-secondary',
+      'toggle-success',
+      'toggle-warning',
+      'toggle-error',
+      'toggle-neutral',
+      'toggle-info',
+      'toggle-accent'
+    );
+  }
+  // this element does not have vairants
+  @Input({ alias: 'variant' }) __variant__: 'checkbox' | 'radio' = 'checkbox';
+  protected override updateVariant(variant: 'ghost' | 'bordered'): void {}
 }
